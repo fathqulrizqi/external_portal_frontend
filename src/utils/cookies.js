ba97c96b4e4fb524230e44399ace453b384b4d41
adisti.putri <adisti.putri@NB-IT0009>
2025-12-12 15:14:15 +0700

    login

--- src/utils/cookies.js
+++ src/utils/cookies.js
@@ -2,7 +2,7 @@ import Cookies from "universal-cookie";
 
 const cookies = new Cookies();
 
-export const setToken = (token) => cookies.set("token", token);
+export const setToken = (token) => cookies.set("token", token, { path: "/" });
 export const getToken = () => cookies.get("token");
 export const removeToken = () => cookies.remove("token");
 
