7c26478d85063e76fe835a12be65ef91da8d292a
adisti.putri <adisti.putri@NB-IT0009>
2025-12-17 13:09:17 +0700

    bearer token

--- src/api/auth.js
+++ src/api/auth.js
@@ -126,7 +126,7 @@ export const OTPRegistrationVerification = async ({ otp }) => {
       { otp },
       {
         headers: {
-          Authorization: `${token}`,
+          Authorization: `Bearer ${token}`,
         },
       }
     );
